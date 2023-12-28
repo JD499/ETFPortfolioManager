eb01594c35698a4e8426801e94f6afa4ee8d488f
galileomir <jd499b@gmail.com>
2023-12-27 20:28:07 -0800

    Refactor CSV parsing and portfolio calculation functions
    The CSV parsing feature has been refactored, introducing process_csv method for improved CSV reading, processing, and ETF dictionary creation. Additionally, new route handlers for CSV file upload and portfolio calculation have been added. The calculate_etf_portfolio method has also been updated for better readability and comprehension, with detailed docstrings added for each function.

--- app.py
+++ app.py
@@ -1,89 +1,133 @@
+from io import StringIO
 import pandas as pd
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    """
+    Returns a greeting message.
+
+    :return: a string containing the message "Hello World!"
+    """
     return 'Hello World!'
 
 
-def add_etf_holdings_from_csv(etf_holdings_dict, file_path):
-    cusip = file_path.split('.')[0]
+etf_holdings_dict = {}
 
-    # Read the file
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
 
-    # Find the start of the data
+
+def process_csv(file):
+    """
+    Process a CSV file and extract ETF holdings.
+
+    :param file: The file object representing the CSV file.
+    :type file: file-like object
+    :return: A dictionary mapping CUSIPs to their corresponding weights in the ETF.
+    :rtype: dict
+    :raises ValueError: If the data header is not found in the file.
+    """
+    file_content = file.read().decode('utf-8')
+    lines = file_content.split('\n')
     start_of_data = None
     for i, line in enumerate(lines):
         if line.startswith('ISSUER,CUSIP'):
             start_of_data = i
             break
-
     if start_of_data is None:
         raise ValueError("Data header not found in the file")
+    csv_data = '\n'.join(lines[start_of_data:])
+    df = pd.read_csv(StringIO(csv_data))
+    df['WEIGHT'] = pd.to_numeric(df['WEIGHT'].str.rstrip('%'), errors='coerce') / 100.0
+    etf_holdings = df.set_index('CUSIP')['WEIGHT'].to_dict()
+    return etf_holdings
 
-    # Read the holdings data
-    holdings_df = pd.read_csv(file_path, skiprows=start_of_data)
-    holdings_df['WEIGHT'] = pd.to_numeric(holdings_df['WEIGHT'].str.rstrip('%'), errors='coerce') / 100.0
 
-    # Create the dictionary for this ETF
-    etf_holdings = {}
-    for _, row in holdings_df.iterrows():
-        stock_name = row['CUSIP']
-        stock_weight = row['WEIGHT']
-        etf_holdings[stock_name] = stock_weight
+def add_etf_holdings_from_csv(etf_holdings_dict, file, cusip):
+    """
+    Add ETF holdings from a CSV file to the provided dictionary.
 
+    :param etf_holdings_dict: A dictionary to store the ETF holdings.
+    :type etf_holdings_dict: dict
+    :param file: The path to the CSV file containing the ETF holdings.
+    :type file: str
+    :param cusip: The CUSIP (Committee on Uniform Securities Identification Procedures) for the ETF.
+    :type cusip: str
+    :return: None
+
+
+    This method reads the CSV file, processes it, and adds the ETF holdings to the provided dictionary
+    using the specified CUSIP as the key.
+
+    Example:
+    >>> etf_holdings_dict = {'ABC123': {'StockA': 100, 'StockB': 150}}
+    >>> add_etf_holdings_from_csv(etf_holdings_dict, '/path/to/etf_holdings.csv', 'XYZ456')
+    >>> print(etf_holdings_dict)
+    {'ABC123': {'StockA': 100, 'StockB': 150}, 'XYZ456': {'StockC': 200, 'StockD': 300}}
+    """
+    etf_holdings = process_csv(file)
     etf_holdings_dict[cusip] = etf_holdings
 
 
-# Usage example
-etf_holdings_dict = {}  # Initialize an empty dictionary
-add_etf_holdings_from_csv(etf_holdings_dict, '025072356.csv')
-add_etf_holdings_from_csv(etf_holdings_dict, '025072885.csv')
-add_etf_holdings_from_csv(etf_holdings_dict, '025072877.csv')
+@app.route('/upload-csv', methods=['POST'])
+def upload_csv():
+    """
+    Uploads a CSV file and adds ETF holdings to the etf_holdings_dict.
+
+    :return: A JSON response indicating the successful upload and processing of the file.
+    """
+    file = request.files['file']
+    cusip = request.form['cusip']
+    add_etf_holdings_from_csv(etf_holdings_dict, file, cusip)
+    return jsonify({"message": "File uploaded and processed successfully"})
+
+
+@app.route('/calculate-portfolio', methods=['POST'])
+def calculate_portfolio():
+    """
+    Calculate portfolio based on the ETF information provided in the request body.
+
+    :return: A JSON response containing the sorted list of tuples containing the CUSIP and their corresponding percentage of the total portfolio value.
+    """
+    etf_info = request.json
+    sorted_stock_values = calculate_etf_portfolio(etf_info, etf_holdings_dict)
+    return jsonify(sorted_stock_values)
 
 
 def calculate_etf_portfolio(etf_info, stock_weights):
     """
-    Calculate the value of individual stocks in a portfolio of ETFs.
+    Calculate ETF Portfolio
 
-    :param etf_info: A dictionary containing the price and shares of each ETF.
-                     For example: {'ETF One': {'price': 50, 'shares': 2}, 'ETF Two': {'price': 100, 'shares': 1}}
-    :param stock_weights: A dictionary of dictionaries containing the weights of stocks in each ETF.
-                          For example: {'ETF One': {'AAPL': 0.8, 'MSFT': 0.2}, 'ETF Two': {'AAPL': 0.25, 'MSFT': 0.25, 'TSLA': 0.25, 'AMZN': 0.25}}
-    :return: A sorted list of tuples with stock names and their percentage of the total portfolio value.
+    :param etf_info: A dictionary containing information about the ETFs in the portfolio.
+                      The keys of the dictionary are the ETF names, and the values are dictionaries
+                      with keys 'price' and 'shares'. 'price' represents the current price of the ETF,
+                      and 'shares' represents the number of shares held in the portfolio.
+                      Example: {'etf1': {'price': 100, 'shares': 50}, 'etf2': {'price': 150, 'shares': 30}}
+
+    :param stock_weights: A nested dictionary containing the weightage of each stock in each ETF.
+                          The keys of the outer dictionary are the ETF names, and the values are dictionaries
+                          with keys as stock names, and corresponding values as the weightage of the stock in that ETF.
+                          Example: {'etf1': {'stock1': 0.4, 'stock2': 0.6}, 'etf2': {'stock3': 0.7, 'stock4': 0.3}}
+
+    :return: A sorted list of tuples containing the stock names and their corresponding percentage of the total portfolio value.
+             The list is sorted in descending order of stock percentage.
+             Example: [('stock2', 50.0), ('stock3', 30.0), ('stock1', 20.0)]
     """
-    # Initialize the portfolio value and stock values
     portfolio_value = 0
     stock_values = {}
-
-    # Calculate the value of each ETF and add it to the total portfolio value
     for etf, info in etf_info.items():
         etf_value = info['price'] * info['shares']
         portfolio_value += etf_value
-
-        # For each stock in the ETF, calculate its value based on the ETF value and weight
         for stock, weight in stock_weights[etf].items():
             if stock not in stock_values:
                 stock_values[stock] = 0
             stock_values[stock] += etf_value * weight
-
-    # Calculate the percentage of each stock's value in relation to the total portfolio value
     stock_percentages = {stock: value / portfolio_value * 100 for stock, value in stock_values.items()}
-
-    # Sort the stock values by their percentage of the total portfolio value
     sorted_stock_values = sorted(stock_percentages.items(), key=lambda item: item[1], reverse=True)
-
     return sorted_stock_values
 
-
-# Example usage:
-etf_info_example = {'025072356': {'price': 50, 'shares': 2}, '025072885': {'price': 50, 'shares': 2},
-    '025072877': {'price': 100, 'shares': 1}}
 
 if __name__ == '__main__':
     app.run()
